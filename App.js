da5c8db53f8fe1ad2c70562e5891d2df3aae1aae
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-03-19 12:12:34 +0000

    chore: Added update the text on input

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=683042a2';
+  const [state, setState] = useState({
+    s: 'Enter a movie',
+    results: [],
+    selected: {}
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Hello World - React native</Text>
+      <Text style={styles.title}>Movie DB</Text>
+      <TextInput 
+        style={styles.searchbox}
+        onChangeText={text => setState(prevState => {
+          return {...prevState, s: text}
+        })}
+        value={state.s}
+      />
     </View>
   );
 }
@@ -13,8 +27,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#223343',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 70,
+    paddingHorizontal: 20
   },
+  title: {
+    color: '#FFFFFF',
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20
+  },
+  searchbox: {
+    fontSize: 20,
+    fontWeight: '300',
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    marginBottom: 40
+  }
 });
